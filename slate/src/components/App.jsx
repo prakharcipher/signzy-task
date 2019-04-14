e96908a2b34ffd44b46b48b689b48aaabc4c5d05
Prakhar <prakhar@farmguide.in>
2019-04-14 18:12:13 +0530

    Intro and authentication tabs done, stylesheet created

--- slate/src/components/App.jsx
+++ slate/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import {Row, Col, Nav, Form} from 'react-bootstrap';
+import {Row, Col, Nav, Form, Tabs, Tab} from 'react-bootstrap';
 import MidBody from './MidBody';
+import './style.css';
 
 export default class App extends Component {
 	render() {
@@ -18,7 +19,62 @@ export default class App extends Component {
 						  </ul>
 						</Nav>
 					</Col>
-					<Col lg={10} xs={12}><MidBody /></Col>
+					<Col lg={10} xs={12}>
+						<Row>
+							<Col style={{backgroundColor: '#f2f2f2'}} lg={6} xs={12}>
+								<h3 style={{padding: '1%', backgroundColor: 'white'}} >Introduction</h3>
+								<div style={{padding: '4%'}}>
+									<p>
+										Welcome to the Kittn API! You can use our API to access Kittn API endpoints, which can get information on various cats, kittens, and breeds in our database.
+									</p>
+									<p>
+										We have language bindings in Shell, Ruby, Python, and JavaScript! You can view code examples in the dark area to the right, and you can switch the programming language of the examples with the tabs in the top right.
+									</p>
+									<p>
+										This example API documentation page was created with Slate. Feel free to edit it and use it as a base for your own API's documentation.
+									</p>
+								</div>
+							</Col>
+							<Col style={{backgroundColor: '#2E3336'}} lg={6} xs={12}>
+								<Tabs defaultActiveKey="shell" id="uncontrolled-tab-example">
+								  <Tab eventKey="shell" title="Shell">
+								  </Tab>
+								  <Tab eventKey="ruby" title="Ruby">
+								  </Tab>
+								  <Tab eventKey="python" title="Python">
+								  </Tab>
+								  <Tab eventKey="javascript" title="JavaScript">
+								  </Tab>
+								</Tabs>
+							</Col>
+						</Row>
+						<Row>
+							<Col style={{backgroundColor: '#f2f2f2'}} lg={6} xs={12}>
+								<h3 style={{padding: '1%', backgroundColor: 'white'}} >Authentication</h3>
+								<div style={{padding: '4%'}}>
+									<p>
+										Kittn uses API keys to allow access to the API. You can register a new Kittn API key at our <a href="http://example.com/developers">developer portal</a>.
+									</p>
+									<p>
+										Kittn expects for the API key to be included in all API requests to the server in a header that looks like the following:
+									</p>
+									<p>
+										<code>Authorization: meowmeowmeow</code>
+									</p>
+								</div>
+							</Col>
+							<Col style={{backgroundColor: '#2E3336', paddingLeft: '0'}} lg={6} xs={12}>
+								<div>
+									<p style={{backgroundColor: '#191D1F', color: '#EEEEEE', padding: '13px 2em'}}>To authorize, use this code:</p>
+									<div style={{padding: '13px 2em', color: 'white'}}>
+										<p><span className="keyword">const</span> kittn = require(<span className="str">'kittn'</span>)</p>
+										<p>let api = kittn.authorize('meowmeowmeow')</p>
+									</div>
+									<p style={{backgroundColor: '#191D1F', color: '#EEEEEE', padding: '13px 2em'}}>Make sure to replace <em>meowmeowmeow</em> with your API key.</p>
+								</div>
+							</Col>
+						</Row>
+					</Col>
 				</Row>
 			)
 	}
