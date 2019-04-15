ead2999b4e0fd3a178bc454fc4d9d721932e68e6
Prakhar <prakhar@farmguide.in>
2019-04-15 16:24:05 +0530

    final touch ups

--- slate/src/components/App.jsx
+++ slate/src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {Row, Col, Nav, Form, Tabs, Tab} from 'react-bootstrap';
-import MidBody from './MidBody';
 import ScrollableAnchor from 'react-scrollable-anchor';
 import { goToAnchor } from 'react-scrollable-anchor'
 import './style.css';
@@ -30,7 +29,7 @@ export default class App extends Component {
 		}
 	}
 
-	handleClick = (e) => {
+	handleClick = (e) => {					
 		const listType = e.target.innerHTML;
 		if(listType === 'Introduction')
 			goToAnchor('intro', false);
@@ -45,7 +44,7 @@ export default class App extends Component {
 		let jsAuth = (
 				<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
 					<p><span className="keyword">const</span> kittn = require(<span className="str">'kittn'</span>)</p>
-					<p><span className="keyword">let</span> api = kittn.authorize('meowmeowmeow')</p>
+					<p><span className="keyword">let</span> api = kittn.authorize(<span className="str">'meowmeowmeow'</span>)</p>
 				</div>
 			);
 
@@ -70,8 +69,8 @@ export default class App extends Component {
 				</div>
 			);
 		return (
-				<Row>
-					<Col className="hidden-xs" style={{backgroundColor: '#2E3336', color: 'white', zIndex: '30', position: 'fixed', left: '0', top: '0', height: '100vh'}} lg={2}>
+				<Row>								
+					<Col className="hidden-xs" style={{backgroundColor: '#2E3336', color: 'white', position: 'fixed', left: '0', top: '0', height: '100vh'}} lg={2}>
 						<img style={{width: '100%', display: 'block', margin: 'auto'}} alt="logo" src="https://lord.github.io/slate/images/logo.png" /><br />
 						<Form.Control style={{width: '90%', margin: 'auto', backgroundColor: '#2E3336', color: 'gray'}} type="text" placeholder="%search%" /><br/>
 						<Nav defaultActiveKey="/home" className="flex-column">
@@ -132,10 +131,6 @@ export default class App extends Component {
 							<Col style={{backgroundColor: '#2E3336', paddingLeft: '0'}} lg={6} xs={12}>
 								<div style={{marginTop: '10%'}}>
 									<p style={{backgroundColor: '#191D1F', color: '#EEEEEE', padding: '13px 2em', margin: '0'}}>To authorize, use this code:</p>
-									{/*<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
-										<p><span className="keyword">const</span> kittn = require(<span className="str">'kittn'</span>)</p>
-										<p>let api = kittn.authorize('meowmeowmeow')</p>
-									</div>*/}
 									{key === 'javascript' ? jsAuth : pyAuth}
 									<p style={{backgroundColor: '#191D1F', color: '#EEEEEE', padding: '13px 2em'}}>Make sure to replace <em>meowmeowmeow</em> with your API key.</p>
 								</div>
@@ -181,10 +176,6 @@ export default class App extends Component {
 							</ScrollableAnchor>
 							<Col style={{backgroundColor: '#2E3336', paddingLeft: '0'}} lg={6} xs={12}>
 								<div style={{marginTop: '15%'}}>
-									{/*<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
-										<p><span className="keyword">const</span> kittn = require(<span className="str">'kittn'</span>)</p>
-										<p><span className="keyword">let</span> api = kittn.authorize(<span className="str">'meowmeowmeow'</span>)<br /><span className="keyword">let</span> kittens = api.kittens.get();</p>
-									</div>*/}
 									{key=== 'javascript' ? jsKitten : pyKitten}
 									<p style={{backgroundColor: '#191D1F', color: '#EEEEEE', padding: '13px 2em', margin: '0'}}>The above command returns JSON structured like this:</p>
 									<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
