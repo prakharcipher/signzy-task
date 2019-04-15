550a0137256498c0a69f34b5692d3ad041f17031
Prakhar <prakhar@farmguide.in>
2019-04-15 15:43:46 +0530

    code language compatibility added for python and javascript

--- slate/src/components/App.jsx
+++ slate/src/components/App.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Row, Col, Nav, Form, Tabs, Tab} from 'react-bootstrap';
 import MidBody from './MidBody';
+import ScrollableAnchor from 'react-scrollable-anchor';
+import { goToAnchor } from 'react-scrollable-anchor'
 import './style.css';
 
 const data = [
@@ -21,22 +23,68 @@ const data = [
 ];
 
 export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			key: 'javascript'
+		}
+	}
+
+	handleClick = (e) => {
+		const listType = e.target.innerHTML;
+		if(listType === 'Introduction')
+			goToAnchor('intro', false);
+		else if(listType === 'Authentication')
+			goToAnchor('auth', false);
+		else
+			goToAnchor('kitten', false);
+	}
+
 	render() {
+		const {key} = this.state;
+		let jsAuth = (
+				<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
+					<p><span className="keyword">const</span> kittn = require(<span className="str">'kittn'</span>)</p>
+					<p><span className="keyword">let</span> api = kittn.authorize('meowmeowmeow')</p>
+				</div>
+			);
+
+		let jsKitten = (
+				<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
+					<p><span className="keyword">const</span> kittn = require(<span className="str">'kittn'</span>)</p>
+					<p><span className="keyword">let</span> api = kittn.authorize(<span className="str">'meowmeowmeow'</span>)<br /><span className="keyword">let</span> kittens = api.kittens.get();</p>
+				</div>
+			);
+
+		let pyAuth = (
+				<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
+					<p><span className="keyword-py">import</span> kittn</p>
+					<p>api = kittn.authorize(<span className="str">'meowmeowmeow'</span>)</p>
+				</div>
+			);
+
+		let pyKitten = (
+				<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
+					<p><span className="keyword-py">import</span> kittn</p>
+					<p>api = kittn.authorize(<span className="str">'meowmeowmeow'</span>)<br />api.kittens.get();</p>
+				</div>
+			);
 		return (
 				<Row>
 					<Col className="hidden-xs" style={{backgroundColor: '#2E3336', color: 'white', zIndex: '30', position: 'fixed', left: '0', top: '0', height: '100vh'}} lg={2}>
 						<img style={{width: '100%', display: 'block', margin: 'auto'}} alt="logo" src="https://lord.github.io/slate/images/logo.png" /><br />
 						<Form.Control style={{width: '90%', margin: 'auto', backgroundColor: '#2E3336', color: 'gray'}} type="text" placeholder="%search%" /><br/>
 						<Nav defaultActiveKey="/home" className="flex-column">
-						  <ul style={{listStyleType: 'none'}}>
-						  	<li>Introduction</li>
-							  <li>Authentication</li>
-							  <li>Kittens</li>
+						  <ul style={{listStyleType: 'none'}} onClick={this.handleClick}>
+						  	<li value="intro">Introduction</li>
+							  <li value="auth">Authentication</li>
+							  <li value="kitten">Kittens</li>
 						  </ul>
 						</Nav>
 					</Col>
 					<Col lg={{size: 10, offset: 2}} xs={12} >
 						<Row>
+						<ScrollableAnchor id={'intro'}>
 							<Col style={{backgroundColor: '#f2f2f2'}} lg={6} xs={12}>
 								<h3 style={{padding: '1%', backgroundColor: 'white', border: '1px solid #ccc'}} >Introduction</h3>
 								<div style={{padding: '4%'}}>
@@ -51,20 +99,18 @@ export default class App extends Component {
 									</p>
 								</div>
 							</Col>
+							</ScrollableAnchor>
 							<Col style={{backgroundColor: '#2E3336'}} lg={6} xs={12}>
-								<Tabs style={{position: 'fixed', right: '0', top: '0', width: '41.5%'}} defaultActiveKey="shell" id="uncontrolled-tab-example">
-								  <Tab eventKey="shell" title="Shell">
-								  </Tab>
-								  <Tab eventKey="ruby" title="Ruby">
+								<Tabs style={{position: 'fixed', right: '0', top: '0', width: '41.5%'}} defaultActiveKey={this.state.key} id="uncontrolled-tab-example" onSelect={key => this.setState({ key })}>
+								  <Tab eventKey="javascript" title="JavaScript">
 								  </Tab>
 								  <Tab eventKey="python" title="Python">
-								  </Tab>
-								  <Tab eventKey="javascript" title="JavaScript">
 								  </Tab>
 								</Tabs>
 							</Col>
 						</Row>
 						<Row>
+						<ScrollableAnchor id={'auth'}>
 							<Col style={{backgroundColor: '#f2f2f2'}} lg={6} xs={12}>
 								<h3 style={{padding: '1%', backgroundColor: 'white', border: '1px solid #ccc'}} >Authentication</h3>
 								<div style={{padding: '4%'}}>
@@ -82,18 +128,21 @@ export default class App extends Component {
 									</aside>
 								</div>
 							</Col>
+							</ScrollableAnchor>
 							<Col style={{backgroundColor: '#2E3336', paddingLeft: '0'}} lg={6} xs={12}>
-								<div>
-									<p style={{backgroundColor: '#191D1F', color: '#EEEEEE', padding: '13px 2em', marginTop: '10%'}}>To authorize, use this code:</p>
-									<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
+								<div style={{marginTop: '10%'}}>
+									<p style={{backgroundColor: '#191D1F', color: '#EEEEEE', padding: '13px 2em', margin: '0'}}>To authorize, use this code:</p>
+									{/*<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
 										<p><span className="keyword">const</span> kittn = require(<span className="str">'kittn'</span>)</p>
 										<p>let api = kittn.authorize('meowmeowmeow')</p>
-									</div>
+									</div>*/}
+									{key === 'javascript' ? jsAuth : pyAuth}
 									<p style={{backgroundColor: '#191D1F', color: '#EEEEEE', padding: '13px 2em'}}>Make sure to replace <em>meowmeowmeow</em> with your API key.</p>
 								</div>
 							</Col>
 						</Row>
 						<Row>
+						<ScrollableAnchor id={'kitten'}>
 							<Col style={{backgroundColor: '#f2f2f2'}} lg={6} xs={12}>
 								<h3 style={{padding: '1%', backgroundColor: 'white', border: '1px solid #ccc'}} >Kittens</h3>
 								<h5 style={{padding: '4%'}}>Get All Kittens</h5>
@@ -129,12 +178,14 @@ export default class App extends Component {
 									</aside>
 								</div>			
 							</Col>
+							</ScrollableAnchor>
 							<Col style={{backgroundColor: '#2E3336', paddingLeft: '0'}} lg={6} xs={12}>
 								<div style={{marginTop: '15%'}}>
-									<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
+									{/*<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
 										<p><span className="keyword">const</span> kittn = require(<span className="str">'kittn'</span>)</p>
 										<p><span className="keyword">let</span> api = kittn.authorize(<span className="str">'meowmeowmeow'</span>)<br /><span className="keyword">let</span> kittens = api.kittens.get();</p>
-									</div>
+									</div>*/}
+									{key=== 'javascript' ? jsKitten : pyKitten}
 									<p style={{backgroundColor: '#191D1F', color: '#EEEEEE', padding: '13px 2em', margin: '0'}}>The above command returns JSON structured like this:</p>
 									<div style={{padding: '13px 2em', color: 'white', backgroundColor: '#1E2224'}}>
 										<pre style={{backgroundColor: '#1E2224', border: '0', color: '#E6DB74'}}>
